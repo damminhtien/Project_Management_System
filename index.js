0dcc56d2a3f58927a57fd09541b3c8bf4c2448b3
damminhtien <damminhtienchl@gmail.com>
2017-09-13 22:55:46 +0700

    paging 13/09

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(session({
 }));
  
 app.use(flash());
-// var connect = require('connect');
 var cookieParser = require('cookie-parser');
 
 app.use(express.static("./public"));
@@ -43,29 +42,28 @@ app.get('/', function(req, res){
         if (err) {
             return console.error('Error acquiring client', err.stack);
         }
-        var rs = {
-            thongbao:null,
-            doan:null
-        };
-        client.query('SELECT * FROM thongbao order by id asc', (err, result) => {
+        client.query('SELECT * FROM thongbao ORDER BY id DESC LIMIT 10', (err, result) => {
             release();
             if (err) {
             	res.end();
                 return console.error('Error executing query', err.stack)
             }
-            rs.thongbao = result;
+            // rs.thongbao = result;
             // console.log(rs);
-            res.render('./home/trangchu', rs);
+            res.render('./home/trangchu', {thongbao: result, usr: req._passport.session});
         })  
     })
 });
 
+/* sử dụng chứng thực local, nếu chứng thực ko đc thì gửi mess*/
 app.route('/login')
 .get(function(req, res){
     res.render('login', {message: req.flash('error')});
 })
 .post(passport.authenticate('local', {failureRedirect:'/login', successRedirect: '/', failureFlash: true}));
 
+
+/* chứng thực local strategy, lấy dữ liệu từ DB, nếu đúng/sai return hàm done(null, usr) / done (null, false) */
 passport.use(new LocalStrategy(
     (username, password, done) => {
         pool.connect((err, client, release) => {
@@ -79,11 +77,11 @@ passport.use(new LocalStrategy(
                 return console.error('Error executing query', err.stack)
             }
             var key = true;
-            result.rows.forEach(function(usr, index){
+            result.rows.forEach(function(usr){
                 if(usr.id == username){
                     if(usr.password == password) {
                         key = false;
-                        return done(null, usr);
+                        return done(null, usr, {usr: usr} );
                     }
                     else{
                         key = false; 
@@ -93,10 +91,49 @@ passport.use(new LocalStrategy(
             });
             if(key == true) return done(null, false, {message: 'Invalid username.'});
         })  
-    })
+        })
     }
 ))
 
+/* nếu chứng thực đúng sẽ gọi  */
 passport.serializeUser((user, done) => {
     done(null, user);
 })
+
+passport.deserializeUser(function(user, done) {
+    pool.connect((err, client, release) => {
+        if (err) {
+            return console.error('Error acquiring client', err.stack);
+        }
+        client.query('SELECT * FROM sinhvien', (err, result) => {
+            release();
+            if (err) {
+                res.end();
+                return console.error('Error executing query', err.stack)
+            }
+            result.rows.forEach(function(usr){
+                if(usr.id == user){
+                    return done(null, usr);
+                }
+            });
+        })  
+    })
+});
+
+/* ajax */
+app.get("/recent/post=:id.json", function(req, res){
+    var id = parseInt(req.params.id);
+    pool.connect((err, client, release) => {
+        if (err) {
+            return console.error('Error acquiring client', err.stack);
+        }
+        client.query('SELECT * FROM thongbao ORDER BY id DESC OFFSET ' + id*8 +' LIMIT 8', (err, result) => {
+            release();
+            if (err) {
+                res.end();
+                return console.error('Error executing query', err.stack)
+            }
+            res.send(result.rows);
+        })  
+    })
+});
--- public/js/trangchu.js
+++ public/js/trangchu.js
@@ -1,4 +1,5 @@
 $("document").ready(function(){
+	var post = 0;
 	$("#inputSearch").focusin(function(){
 		$(this).css("background-color", "#f0f5f5");
 		$("#div_search_tool form").css({"width":"60%","transition":"2s"});
@@ -8,5 +9,58 @@ $("document").ready(function(){
 		$(this).css("background-color", "#fff");
 		$("#div_search_tool form").css({"width":"50%","transition":"2s"});
 		$("#btnSearch").css("background-color", "#fff");
+	})
+	$("#btnPageNext").click(function(){
+		post++;
+		var url = "/recent/post="+post+".json";
+		$.get(url).then(function(data,){
+  			for(var i=0; i<8; i++){
+  				var postTitle = "#postTitle" + i,
+  				 	postBody = "#postBody" + i,
+  					postView = "#postView" + i,
+  					postDate = "#postDate" + i,
+  					postTag = "#postTag" + i;
+  				$(postTitle).html(data[i].tieude);
+				$(postBody).html(data[i].tomtatnoidung);
+				$(postView).html(data[i].luotxem);
+				$(postDate).html(data[i].ngaygioupload);
+  			}
+		});
+	})
+	$("#btnPagePrev").click(function(){
+		if(post>=1){
+			post--;
+			var url = "/recent/post="+post+".json";
+			$.get(url).then(function(data,){
+	  			for(var i=0; i<8; i++){
+	  				var postTitle = "#postTitle" + i,
+	  				 	postBody = "#postBody" + i,
+	  					postView = "#postView" + i,
+	  					postDate = "#postDate" + i,
+	  					postTag = "#postTag" + i;
+	  				$(postTitle).html(data[i].tieude);
+					$(postBody).html(data[i].tomtatnoidung);
+					$(postView).html(data[i].luotxem);
+					$(postDate).html(data[i].ngaygioupload);
+	  			}
+			});
+		}
+	})
+	$("#btnPage1").click(function(){
+		post = 0;
+		var url = "/recent/post="+post+".json";
+		$.get(url).then(function(data,){
+				for(var i=0; i<8; i++){
+					var postTitle = "#postTitle" + i,
+					 	postBody = "#postBody" + i,
+						postView = "#postView" + i,
+						postDate = "#postDate" + i,
+						postTag = "#postTag" + i;
+					$(postTitle).html(data[i].tieude);
+					$(postBody).html(data[i].tomtatnoidung);
+					$(postView).html(data[i].luotxem);
+					$(postDate).html(data[i].ngaygioupload);
+				}
+		});
 	})
 });
