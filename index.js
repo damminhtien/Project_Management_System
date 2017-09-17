742ef52aa7ad1af7e3676b953760ba5fb14cb2cd
damminhtien <damminhtienchl@gmail.com>
2017-09-18 01:00:10 +0700

    18/09 xong dang nhap

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var session = require('express-session');
 var flash = require('connect-flash-plus');
- 
+var fs = require('fs');
+
 app.use(session({
   secret: 'keyboard cat',
-  cookie: { maxAge: 60000 }
+  cookie: { maxAge: 60000 },
+  saveUninitialized: true,
+  resave: true
 }));
  
 app.use(flash());
@@ -18,10 +21,11 @@ app.use(express.static("./public"));
 app.set("view engine","ejs");
 app.set("views","./views");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(passport.initialize());
 app.use(cookieParser());
 app.use(flash());
+app.use(passport.session());
 
 var pg = require("pg");
 const pool = new pg.Pool({
@@ -55,13 +59,51 @@ app.get('/', function(req, res){
     })
 });
 
+app.get('/logout', (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+});
+
+app.get('/sinhvien', function(req, res){
+    // var id = req._parsedUrl.query;
+    // console.log(req.session.passport.user);
+    if(req.isAuthenticated()){
+        pool.connect((err, client, release) => {
+        if (err) {
+            return console.error('Error acquiring client', err.stack);
+        }
+        client.query('SELECT * FROM sinhvien WHERE id='+ req.session.passport.user.id, (err, result) => {
+            release();
+            if (err) {
+                res.end();
+                return console.error('Error executing query', err.stack)
+            }
+            res.render('./sinhvien/sinhvien', {usr: req.session.passport.user});
+        })  
+        })
+    }
+    else{
+        res.redirect('/');
+    }
+    
+});
+
+    app.get('/da', function(req, res){
+    var filePath = "/uploads/DA1.pdf";
+
+    fs.readFile(__dirname + filePath , function (err,data){
+        res.contentType("application/pdf");
+        res.send(data);
+    });
+})
+
 /* sử dụng chứng thực local, nếu chứng thực ko đc thì gửi mess*/
 app.route('/login')
 .get(function(req, res){
     res.render('login', {message: req.flash('error')});
 })
 .post(passport.authenticate('local', {failureRedirect:'/login', successRedirect: '/', failureFlash: true}));
-
 
 /* chứng thực local strategy, lấy dữ liệu từ DB, nếu đúng/sai return hàm done(null, usr) / done (null, false) */
 passport.use(new LocalStrategy(
@@ -97,10 +139,12 @@ passport.use(new LocalStrategy(
 
 /* nếu chứng thực đúng sẽ gọi  */
 passport.serializeUser((user, done) => {
+    // console.log("serializeUser" + user);
     done(null, user);
 })
 
 passport.deserializeUser(function(user, done) {
+    // console.log('deserializeUser');
     pool.connect((err, client, release) => {
         if (err) {
             return console.error('Error acquiring client', err.stack);
@@ -112,8 +156,9 @@ passport.deserializeUser(function(user, done) {
                 return console.error('Error executing query', err.stack)
             }
             result.rows.forEach(function(usr){
-                if(usr.id == user){
-                    return done(null, usr);
+                if(usr.id == user.id){
+                    // console.log(user);
+                    return done(null, user);
                 }
             });
         })  
@@ -137,3 +182,12 @@ app.get("/recent/post=:id.json", function(req, res){
         })  
     })
 });
+
+// function checkAuthentication(req,res,next){
+//     if(req.isAuthenticated()){
+//         //if user is loged in, req.isAuthenticated() will return true 
+//         next();
+//     } else{
+//         res.redirect("/login");
+//     }
+// }
